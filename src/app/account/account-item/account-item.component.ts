05feee9200d0c75e45ed59ae7ceae277fadadb30
= <adam.stevens@investecwin.co.uk>
2017-04-21 16:58:07 +0100

    fix for payments only happening for credits

--- src/app/account/account-item/account-item.component.ts
+++ src/app/account/account-item/account-item.component.ts
@@ -105,9 +105,9 @@ export class AccountItemComponent implements OnInit, OnChanges, OnDestroy {
         if (date <= today) {
           m.Payments[m.Payments.length -1].isPending = false;                    
           var payment = m.Payments[m.Payments.length -1];          
-          if (m.isCredit) {            
-            this.addPaymentToAccount(payment, m.isCredit);
-          } 
+          
+          this.addPaymentToAccount(payment, m.isCredit);
+          
           var nextPayDate = new Date(today.getFullYear(), today.getMonth() + 1, today.getDate());
           nextPayDate.setDate(m.DayOfMonth);
           nextPayDate.setHours(1,0,0,0);
